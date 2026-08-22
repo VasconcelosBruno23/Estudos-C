8a65f0bcd79073d8c98b6d88b2148258aebc5a57
Bruno Vasconcelos <brunootaviovasconcelos@gmail.com>
2026-08-22 10:39:30 -0300

    Adicionando primeiro programa interativo.

---
+++ primeirointerativo.c
@@ -0,0 +1,15 @@
+//Primeiro Programa Interativo
+
+#include <stdio.h>
+
+int main()
+{
+  int idade;
+
+  printf("Digite a sua idade: ");
+  scanf("%d" ,&idade);
+
+  printf("Voce tem %d anos." ,idade);
+
+  return 0;
+}
